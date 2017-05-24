8ccda5febab150b16efa5cfc3536e50fdb1b1bfe
erdbeerfleisch <info@erdbeerfleisch.de>
2017-05-24 21:51:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Trello Clone
+
+For educational purposes I am trying to clone a trello board.
---
+++ script.js
@@ -0,0 +1,36 @@
+//My Modal
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that closes the modal
+var btn = document.getElementById("closeModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// open the modal when the site is loaded
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on the button, close the modal
+btn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
